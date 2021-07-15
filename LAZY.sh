4819885c5b33f2acb07e78530ad64c34fe032b4f
biothomme <55461592+biothomme@users.noreply.github.com>
2021-07-15 21:24:32 +0200

    even more lazy now, yields the commands

--- LAZY.sh
+++ LAZY.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #
 # Print commands to start vitrual environment and jupy lab
-echo "-- Copy the lines below to start working --"
+echo "-- Copy the lines below to start working --
+- (or they are also copied automatically on mac) -"
 echo "conda activate env_ranker"
 echo "jupyter lab"
+echo "conda activate env_ranker
+jupyter lab" | pbcopy
