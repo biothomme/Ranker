1b80404435dd7e0c8ff37061ad4864b2f84878c3
biothomme <55461592+biothomme@users.noreply.github.com>
2021-08-16 11:37:02 +0200

    worked on NAIP class

--- crows_nest/database_classes.py
+++ crows_nest/database_classes.py
@@ -2,6 +2,8 @@
 # project on geolocations
 
 from abc import ABCMeta, abstractmethod
+import pickle
+import rtree
 
 from utils import set_directory
 
@@ -34,11 +36,17 @@ class SpatialData(metaclass=ABCMeta):
 
 # class for queries of the NAIP database
 class NAIPData(SpatialData):
+    # The code is strongly inspired by (#REF01):
+    # https://planetarycomputer.microsoft.com/dataset/naip#Blob-Storage-Notebook
+    # Thanks a lot to the authors.
     from multidb_wrapper import set_directory
 
     def __init__(self, tile_size=None, directory=None):
         self.database = "NAIP western europe Azure"
         self.root = set_directory(directory)
+        self.database_dir = os.join(
+                self.root,
+                self.database.lower().replace(" ", "_"))
 
     def run(self, location):
         '''
@@ -49,24 +57,88 @@ class NAIPData(SpatialData):
         # a tile index, which is used in a second step to retrieve the necessary tiles.
         # to load the index, we use a subsequent function.
         self.prepare()
-        query = self.build_query(location)
+        self.location = location
+        queries = self.build_query(location)
         try:
-            self.get_data(query)
+            data_files = self.get_data(queries)
         except:
             print(f"It was not possible to fetch data for location {location}")
+        else:
+            print(f"Data for location {location} was downloaded as "
+                    "{[df for df in data_files] if type(data_files) == list else datafiles}")
+        return # WAS NOT TESTED YET!!!!
 
     def authenticate(self):
+        # TODO # 
         print("logged in")
+
 
     def build_query(self, location, date=None):
         '''
         Construct a query for NAIP database from coordinates and date
         '''
-        return
+        self.location = location
+        # copied code BEGIN
+        intersected_indices = list(self.tile_rtree.intersection(location.bounds))
+        assert len(intersected_indices) > 0, (
+                "Location has no intersections with NAIP tiles.")
+        queries = []
+        tile_intersection = False
 
-    def get_data(self):
-        print(self.database)
+        for idx in intersected_indices:
+            intersected_file = self.tile_index[idx][0]
+            intersected_geom = self.tile_index[idx][1]
+            if intersected_geom.contains(point):
+                tile_intersection = True
+                queries.append(intersected_file)
+        
+        assert tile_intersection, (
+                "Error: there are overlaps with tile index, "
+                "but no tile completely contains selection")   
+        assert len(queries) > 0, "Location has no intersections with NAIP tiles."
+        if len(queries) == 1:
+            return queries[0]
+        else:
+            return queries
+        # copied code END
+
+    def get_data(self, queries):
+        if type(queries) == list:
+            data_files = []
+            for query in queries:
+                data_files.append(self.get_data(query))
+            return data_files
+        else:
+            data_file = query.replace("://", "_").replace("/", "_")
+            try:
+                urllib.request(query, data_file)
+            except:
+                print(f"Error, it was impossible to download data for the query {query}"
+                        " at location {self.location}.")
+            return data_file
 
     def prepare(self):
-        # TODO #
-        if os.exists ...self.root()
+        '''
+        Build NAIP database directory and download the index files if not already
+        existent.
+        '''
+        INDEX_FILES = ["tile_index.dat", "tile_index.idx", "tiles.p"]
+        # initialize db directory
+        if not os.exists(self.database_dir):
+            print(f"The directory for database '{self.database}' will be"
+                    " initialized.")
+        # download tile indices
+        tile_indices_root = os.join(self.database_dir, "tile_indices")
+        tile_indices_paths = [
+                os.join(tile_indices_root, file) for file in INDEX_FILES]
+        for i, tile_index_path in enumerate(tile_indices_paths):
+            if not os.exists(tile_index_path):
+                os.makedirs(tile_index_path)
+                print(f"The tiles index file {tile_index_path}"
+                        " ({i+1}/{len(INDEX_FILES}) was downloaded.")
+        # load index_files (taken from #REF01)
+        self.tile_rtree = rtree.index.Index(
+                os.join(tile_indices_root, "tile_index"))
+        self.tile_index = pickle.load(open(tile_indices_root[2], "rb"))
+        return
+
