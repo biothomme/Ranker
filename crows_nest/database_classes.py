ace0fd6f478058ea59bee15f3ea438718c63b6ed
biothomme <55461592+biothomme@users.noreply.github.com>
2021-11-29 16:23:24 +0100

    updated for intake cat

--- crows_nest/database_classes.py
+++ crows_nest/database_classes.py
@@ -10,13 +10,15 @@ import pickle
 import rtree
 import urllib
 
+from data import cat
+
 from utils import set_directory
 from utils import download_to_path
 from utils import check_locations_and_dates
 from utils import write_csv_row
 
-from source_destination import LocalSourceDest
-from source_destination import RemoteSourceDest
+from reader import LocalReader
+from reader import RemoteReader
 
 # Core abstract class that provides important methods for all subclasses
 class SpatialData(metaclass=ABCMeta):
@@ -27,6 +29,45 @@ class SpatialData(metaclass=ABCMeta):
     with open("remote_sources.json", "r") as rs_file:
         REMOTE_SOURCE = json.load(rs_file)
     
+    # classic init
+    def __init__(self, destination_path=None, remote_url=None, features=[],
+                 source_path=None, copy_local=True, extend_local_cache=False,
+                 silent=False):
+        # prepare database dependent informations.
+        self.load_remote_source_and_index()
+        
+        # print or no print?
+        self.silent = silent
+        
+        # set root and database dir
+        self.set_db_directory(destination_path)
+        
+        # change remote url if provided
+        self.set_remote_url(remote_url)
+        
+        # option to load data from a local source
+        # implemented with keeping `self.base_url`, to allow different query
+        # architecture between local cache or online source
+        self.set_reader(source_path=source_path, 
+                        remote_url=self.remote_url,
+                        copy_local=copy_local,
+                        extend_local_cache=extend_local_cache)
+        
+        # define which datatypes should be fetched 
+        ## TODO should be loaded:
+        ## self.features = {"rgb": False, "ir": False}
+        self.set_features(features)
+        
+        # to avoid overwriting by multidb wrapper we flag if destination was 
+        # manually set by user.
+        self.user_defined = False if destination_path is None else True
+        
+        # load the intake catalog function
+        self.load_catalog()
+
+        return
+    
+            
     # authenticate, if necessary
     @abstractmethod
     def authenticate(self):
@@ -39,9 +80,30 @@ class SpatialData(metaclass=ABCMeta):
     
     # download the data
     @abstractmethod
-    def get_data(self, build_query, file_name, location):
+    def get_data(self, build_query, file_name, location, date_given=None):
         pass  # return data_retrieved
 
+    def prepare(self):
+        '''
+        Prepare local directory to enable database specific download.
+        '''
+        # initialize db directory including a `source` subdirectory
+        # which is important if the directory will later be used as
+        # local source.
+        if not os.path.exists(self.database_dir):  # TODO: self.database_dir is here not initialized
+            print(f"The directory for database `{self.DATABASE}` will be"
+                    " initialized.")
+        
+        # initiate the cache directory if neccessary
+        set_directory(self.datasource.destination.cache_dir,
+                      database_name=None)
+
+        # subsequently we will collect filenames for csvs containing
+        # metainformation of raw (source) data and the extracted 
+        # feature specific data 
+        self.csv_index_files = {}
+        
+        return
 
     # produce query, request and store data
     def run(self, locations, dates=None):
@@ -57,7 +119,7 @@ class SpatialData(metaclass=ABCMeta):
         for idx, (location, dt) in enumerate(zip(locations, dates)):
             file_name = self.make_file_name(idx, self.size)
             queries = self.build_query(location, dt)
-            data_files = self.get_data(queries, file_name, location, dt)
+            data_files = self.get_data(queries, file_name, location, date_given=dt)
             
         return
 
@@ -69,7 +131,7 @@ class SpatialData(metaclass=ABCMeta):
         # by the multibd wrapper
         if multi_db_access and self.user_defined:
             if not self.silent:
-                print(f"For the database `{self.database}` "
+                print(f"For the database `{self.DATABASE}` "
                      "a used defined destination but not the "
                       "multi_db directory will be used.")
             return
@@ -78,14 +140,14 @@ class SpatialData(metaclass=ABCMeta):
         self.root = set_directory(directory)
         self.database_dir = os.path.join(
                 self.root,
-                self.DATABASE.lower().replace(" ", "_"))
+                self.db_name)
         
-        # after setting a new destination, the SourceDest objects need to be updated(if already set)
+        # after setting a new destination, the Reader objects need to be updated(if already set)
         if hasattr(self, "datasource"):
-            if isinstance(self.datasource, RemoteSourceDest):
-                self.set_source_dest(remote_url=self.datasource.url)
+            if isinstance(self.datasource, RemoteReader):
+                self.set_reader(remote_url=self.datasource.url)
             else:
-                self.set_source_dest(source_path=self.datasource.root,
+                self.set_reader(source_path=self.datasource.root,
                                      remote_url=self.datasource.url)
         return
 
@@ -95,10 +157,13 @@ class SpatialData(metaclass=ABCMeta):
         Load remote source path and necessary index files from `remote_sources.json`
         into static class variables.
         '''
-        self.remote_url = self.REMOTE_SOURCE[self.DATABASE]["url"]
-        self.index_url = self.REMOTE_SOURCE[self.DATABASE]["index"]["url"]
-        self.index_files = self.REMOTE_SOURCE[self.DATABASE]["index"]["index_files"]
-        self.features_possible = self.REMOTE_SOURCE[self.DATABASE]["features"]
+        self.get_std_name()
+        
+        self.remote_function = cat[self.db_name]
+        self.remote_url = cat[self.db_name](filename="").urlpath.strip("/")
+        self.index_url = cat[self.db_name].metadata["index_url"]
+        self.index_files = cat[self.db_name].metadata["index_files"]
+        self.features_possible = cat[self.db_name].metadata["features"]
         return
     
     
@@ -109,7 +174,7 @@ class SpatialData(metaclass=ABCMeta):
     # allow to change tilesize
     def set_tile_size(self, tile_size):
         '''
-        Change tile size of object ant hus for requests.
+        Change tile size of object and use for requests.
         '''
         self.tile_size = tile_size
         return
@@ -133,14 +198,14 @@ class SpatialData(metaclass=ABCMeta):
         
 
     # change the url
-    def set_remote_url(self, remote_url):
+    def set_remote_url(self, remote_url=None):
         '''
         Change the URL of the remote source.
         '''
         if remote_url is not None:
-            self.remote_url = remote_url
-            if not self.silent:
-                print(f"URL of remote source for `{self.DATABASE}` was changed to `{self.remote_url}`.")
+            self.remote_url = remote_url.strip("/")
+            if not self.silent : print(
+                f"URL of remote source for `{self.DATABASE}` was changed to `{self.remote_url}`.")
         return
 
     # initialize the csv file which is used to store metainformation
@@ -153,7 +218,7 @@ class SpatialData(metaclass=ABCMeta):
         # we treat this as a special case:
         if database_feature != "cache":
             csv_file_name = self.make_csv_name(
-                self.datasource.destination.destination_path,
+                self.datasource.destination.destination_dir,
                 database_feature
             )
         else:
@@ -183,29 +248,37 @@ class SpatialData(metaclass=ABCMeta):
         
         return csv_file_name
     
-    # set the sourcedest class of the object
-    def set_source_dest(self, source_path=None, remote_url=None, copy_local=True,
+    # set the Reader class of the object
+    def set_reader(self, source_path=None, remote_url=None, copy_local=True,
                        extend_local_cache=False):
         '''
-        Set a local source for data extraction if neccessary files exists. 
+        Set a Reader for data retrieval.
+        
+        If no `source_path` is provided, we use a RemoteReader.
         '''
-        # TODO TEST IT!
+        # with path is given we try to set LocalReader:
         if source_path is not None:
             try:
                 # if the source is not approved, a assertion error will be raised.
-                self.datasource = LocalSourceDest(self, source_path, url=remote_url, copy_local=copy_local,
+                self.datasource = LocalReader(self, source_path, url=remote_url, copy_local=copy_local,
                                                  extend_local_cache=extend_local_cache)
             except AssertionError:
                 pass
             else:
+                if not self.silent : print(
+                    f"For `{self.DATABASE}` access we use a LocalReader.")
                 return
         
-        self.datasource = RemoteSourceDest(self, remote_url)
+        # otherwise we use RemoteReader
+        if not self.silent : print(
+                f"For `{self.DATABASE}` access we use a RemoteReader.")
+        self.datasource = RemoteReader(self, remote_url)
         return
     
     # construct a row for a csv metainformation file
     def make_csv_row(self, metainf_assembler, copied_row_as_list=None,
                      **kwargs):
+        # this is for copied rows
         if copied_row_as_list is not None:
             assert  isinstance(copied_row_as_list, list), (
                 "The copied row is not in the right format. Insert list.")
@@ -214,8 +287,10 @@ class SpatialData(metaclass=ABCMeta):
                 f"amount of columns as the header ({len(metainf_assembler.HEADER)} el.). "
             )
             csv_row = copied_row_as_list
+        # this is to build a row from information stored in kwargs
         elif len(kwargs) > 0:
             csv_row = metainf_assembler.build_row(**kwargs)
+        # this is to make the csv header
         else:
             csv_row = [None] * len(metainf_assembler.HEADER)
         
@@ -223,7 +298,26 @@ class SpatialData(metaclass=ABCMeta):
                 zip(metainf_assembler.HEADER, csv_row)}
         return csv_row_dict
     
-
+    # simplifiy db name for indexing of catalog
+    def get_std_name(self):
+        '''
+        Retrieve standardized name of the database (no blanks).
+        '''
+        self.db_name = self.DATABASE.lower().replace(" ", "_")
+        return
+    
+    def load_catalog(self):
+        '''
+        Load database specific catalog.
+        '''
+        if not hasattr(self, "db_name"):
+            self.get_std_name()
+        self.cat = cat[self.db_name]
+        return
+    
+# end SpatialData
+    
+    
 # metaclass to assemble csv files for metainformation
 class MetAssembler(metaclass=ABCMeta):
     '''
